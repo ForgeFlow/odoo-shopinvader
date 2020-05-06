8cbf9ed684e7b040a1d3fd3aeb6bc24d1555af23
HviorForgeFlow <hector.villarreal@forgeflow.com>
2020-05-06 12:58:12 +0200

    Fixup!

--- shopinvader_delivery_carrier/models/sale.py
+++ shopinvader_delivery_carrier/models/sale.py
@@ -17,6 +17,7 @@ class SaleOrder(models.Model):
     def _compute_shopinvader_available_carrier_ids(self):
         for order in self:
             if not order.shopinvader_backend_id:
+                order.shopinvader_available_carrier_ids = False
                 continue
             order.shopinvader_available_carrier_ids = (
                 order._available_carriers()
