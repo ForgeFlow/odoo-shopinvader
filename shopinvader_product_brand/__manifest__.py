68c521f7d62538973a2893d84f99eeb41f31c1e8
HviorForgeFlow <hector.villarreal@forgeflow.com>
2020-06-22 15:50:46 +0200

    [13.0][ADD] shopinvader_product_brand

---
+++ shopinvader_product_brand/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Shopinvader Product Brand',
+    'description': """
+        Producr Brand integration for shopinvader""",
+    'version': '13.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ForgeFlow S.L.',
+    'website': 'https://forgeflow.com',
+    'depends': ["shopinvader", "product_brand"],
+    'data': ["data/ir_product_export.xml"],
+}
