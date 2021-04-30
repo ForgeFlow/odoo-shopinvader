51f1165be94badd2e4008f5cafc7487bd6e3ae5f
Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>
2021-04-30 14:58:57 +0200

    [ADD] delivery_date on SOL

--- shopinvader_delivery_picking/services/abstract_sale.py
+++ shopinvader_delivery_picking/services/abstract_sale.py
@@ -10,10 +10,14 @@ _logger = logging.getLogger(__name__)
 class AbstractSaleService(AbstractComponent):
     _inherit = "shopinvader.abstract.mail.service"
 
+    def _convert_one_sale(self, sale):
+        res = super()._convert_one_sale(sale)
+        res["picking_policy"] = sale.picking_policy
+        res["commitment_date"] = sale.commitment_date
+        res["expected_date"] = sale.expected_date
+        return res
 
-def _convert_one_sale(self, sale):
-    res = super()._convert_one_sale(sale)
-    res["picking_policy"] = sale.picking_policy
-    res["commitment_date"] = sale.commitment_date
-    res["expected_date"] = sale.expected_date
-    return res
+    def _convert_one_line(self, line):
+        res = super()._convert_one_line(line)
+        res["delivery_date"] = line.delivery_date
+        return res
