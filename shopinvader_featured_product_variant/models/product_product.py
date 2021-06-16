15254b3c54fa10618fa5ece8017f397897036893
Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>
2021-06-16 15:37:52 +0200

    [ADD] hero_variant_uniq contraint: only on hero_variant per product_template

--- shopinvader_featured_product_variant/models/product_product.py
+++ shopinvader_featured_product_variant/models/product_product.py
@@ -9,3 +9,11 @@ class ProductProduct(models.Model):
 
     featured = fields.Boolean(help="Add featured flag on product variant")
     hero_variant = fields.Boolean(help="Add Hero variant flag on product variant")
+
+    _sql_constraints = [
+        (
+            "hero_variant_uniq",
+            "unique(hero_variant, product_tmpl_id)",
+            "Only can exist a hero variant per product template",
+        ),
+    ]
